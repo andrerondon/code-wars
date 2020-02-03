2939b4c3ea01abfa1d615e549c99cc2f65f1933a
andre-rondon <surplus@Andre-macbook-pro.local>
2020-02-03 11:14:53 -0800

    Spell Checker

--- SpellChecker.js
+++ SpellChecker.js
@@ -10,5 +10,14 @@
 // Should return ["sport", "ports"].
 
 function grabscrab(anagram, dictionary) {
-    // Your code here
-  }
+    let toFind = anagram.split('').sort().join();
+    let arr = [];
+    for (let i of dictionary) {
+      if (i.split('').sort().join()===toFind) {
+        arr.push(i)
+      }
+    }
+    return arr;
+}
+
+console.log(grabscrab( "ortsp", ["sport", "parrot", "ports", "matey"] ));
